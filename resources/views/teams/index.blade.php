dac9590980831cf139437736e0c5b1ee93c4db83
lukibeg <lucas.santoscv25@gmail.com>
2025-09-25 23:13:36 -0300

    Changes: Removing text inside of class: description-text.

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -5,7 +5,7 @@
     <div class="title-page flex flex-col justify-center items-center mt-10">
         <h1 class="text-4xl font-bold">Times</h1>
         <div class="description-container text-center mb-4 text-lg text-gray-500">
-            <p class="description-text">Gerencie seus times e seus jogadores</p>
+            <p class="description-text"></p>
         </div>
     </div>
 
