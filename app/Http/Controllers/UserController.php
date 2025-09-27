25a507f15c63035a996f4cb576dcd69bb4a7b837
lukibeg <lucas.santoscv25@gmail.com>
2025-09-27 02:07:09 -0300

    Att: Adicionado função updateProfile para que o usuário possa atualizar os campos de seu usuário.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -53,16 +54,46 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request) {}
+
+    public function updateProfile(Request $request)
     {
-        //
+        $user = Auth::user()->id;
+
+        $validated = $request->validate([
+            'birthdate' => 'date',
+            'position' => 'string|max:255',
+            'about_me' => 'string',
+            'strengths' => 'string',
+            'weaknesses' => 'string',
+            'preferred_foot' => 'string|max:255',
+            'height' => 'numeric',
+            'city' => 'string|max:255',
+            'state' => 'string|max:255',
+            'profile_picture' => 'nullable|image|mimes:jpeg,jpg,svg,png,webp|max:5096',
+        ]);
+
+        if ($request->hasFile('profile_picture')) {
+            $validated['profile_picture'] = $request->file('profile_picture')->store('profile_pictures', 'public');
+        } else {
+            $validated['profile_picture'] = null;
+        }
+
+        try {
+            $user = User::find($user);
+            $user->update($validated);
+            return redirect()->route('users.show', $user->id)->with('success', 'Perfil atualizado com sucesso');
+        } catch (\Exception $e) {
+            return redirect()->route('users.show', $user->id)->with('error', 'Erro ao atualizar perfil: ' . $e->getMessage());
+        }
     }
+
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
-        //
+        //         
     }
 }
