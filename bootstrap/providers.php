e8a5135f029a6df58e7b86dd7dc478d251a8cfcd
lukibeg <lucas.santoscv25@gmail.com>
2025-10-01 22:03:26 -0300

    Update: Fix removing inexistent provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\TeamJoinRequestProvider::class,
-    App\Providers\TeamService::class,
     App\Providers\TeamServiceProvider::class,
 ];
