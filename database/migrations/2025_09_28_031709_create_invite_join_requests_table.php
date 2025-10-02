8a342113d81702715e168962c3747672df01e08b
lukibeg <lucas.santoscv25@gmail.com>
2025-10-01 22:04:48 -0300

    Add: New migration to database

---
+++ database/migrations/2025_09_28_031709_create_invite_join_requests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invite_join_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('invite_by_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('team_id')->constrained()->onDelete('cascade');
+            $table->foreignId('invite_to_id')->constrained('users')->onDelete('cascade');
+            $table->enum('status', ['pending', 'accepted', 'rejected'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invite_join_requests');
+    }
+};
