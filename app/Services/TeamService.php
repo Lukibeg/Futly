f46276f2d54821d915fa263ce36bb01d70e95f8c
lukibeg <lucas.santoscv25@gmail.com>
2025-10-13 22:48:45 -0300

    UPDATE: Created listTeam method in this implementation of TeamRepositoryInterface.

--- app/Services/TeamService.php
+++ app/Services/TeamService.php
@@ -2,25 +2,44 @@
 
 namespace App\Services;
 
+use App\Repositories\Interface\TeamRepositoryInterface;
+
+
 use App\Models\Team;
 use App\Models\User;
-use App\Repositories\TeamRepositoryInterface;
 use Illuminate\Support\Facades\DB;
 
 class TeamService
 {
     public function __construct(protected TeamRepositoryInterface $repositoryInterface) {}
 
+    public function listTeam(?Team $team)
+    {
+
+        $teams = null;
+
+        if (!$team) {
+            $teams = $this->repositoryInterface->listAllTeam();
+        } else {
+            $teams = $this->repositoryInterface->selectTeamsWhereIsDifferentOfMyTeam($team);
+        }
+
+        if ($teams->isEmpty()) {
+            throw new \Exception('Nenhum time cadastrado. Cadastre o seu agora mesmo!', 302);
+        }
+
+        return $teams;
+    }
     public function createTeam(array $data)
     {
         $userOwner = $this->repositoryInterface->findUserById($data['owner_id']);
 
         if ($userOwner->team_id !== null) {
-            throw new \Exception('Você já faz parte de um time!');
+            throw new \Exception('Você já faz parte de um time!', 403);
         }
 
         if (!$userOwner) {
-            throw new \Exception('O usuário dono não existe.');
+            throw new \Exception('O usuário dono não existe.', 403);
         }
 
         $members = $data['members'] ?? [];
@@ -28,7 +47,7 @@ class TeamService
         $members = array_unique($members);
 
         if ($this->repositoryInterface->checkIfUsersAreInTeam($members)) {
-            throw new \Exception('Um ou mais usuários já está em um time.');
+            throw new \Exception('Um ou mais usuários já está em um time.', 403);
         }
 
         return $this->repositoryInterface->createTeam($data);
@@ -38,11 +57,11 @@ class TeamService
     {
 
         if ($user->owner_id === $user->id) {
-            throw new \Exception('O dono não pode sair do time. Você pode apagar o time ou transferir a propriedade.');
+            throw new \Exception('O dono não pode sair do time. Você pode apagar o time ou transferir a propriedade.', 403);
         }
 
         if (!in_array($user->id, $team->members)) {
-            throw new \Exception('Você não é membro deste time.');
+            throw new \Exception('Você não é membro deste time.', 403);
         }
 
         $this->repositoryInterface->leaveTeam($team, $user);
