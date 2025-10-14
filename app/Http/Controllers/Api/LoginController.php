d38e4b6ca03ea9a6dc0924eff41070b553f85f43
lukibeg <lucas.santoscv25@gmail.com>
2025-10-13 22:29:51 -0300

    UPDATE: Removing coments

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -22,36 +22,21 @@ class LoginController extends Controller
             ]
         );
 
-        // 1. Tenta autenticar o usuário com as credenciais validadas.
         if (!Auth::attempt($validated)) {
-            // Se a autenticação falhar, retorna um erro 401 padronizado.
             return response()->json([
                 'message' => 'Credenciais inválidas.'
             ], 401);
         }
 
-        // 2. Se a autenticação for bem-sucedida, busca o usuário no banco.
-        //    Esta é a etapa crucial que estava faltando.
         $user = User::where('name', $request->name)->firstOrFail();
 
-        // 3. Agora com o objeto $user, você pode criar o token.
-        // É uma boa prática dar um nome ao token (ex: nome do dispositivo).
-
-        // 4. Apagamos tokens gerados anteriormente e definimos um tempo de expiração de 1 hora.
         $user->tokens()->delete();
         $token = $user->createToken('auth_token', expiresAt: now()->addHour())->plainTextToken;
 
-        // 4. Retorna o token para o cliente.
         return response()->json([
             'user' => $user,
             'access_token' => $token,
             'token_type' => 'Bearer',
         ]);
-    }
-
-    public function apiShow()
-    {
-        http_response_code(200);
-        return json_encode(['teste' => 'Retornado']);
     }
 }
