66cb57e5c5443740f310ad7c42cc7c1c6eacc6ab
lukibeg <lucas.santoscv25@gmail.com>
2025-10-13 22:38:04 -0300

    UPDATE: Moving TeamRepositoryInterface to Interface folder.

---
+++ app/Repositories/Interface/TeamRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Interface;
+
+use App\Models\Team;
+use App\Models\User;
+
+interface TeamRepositoryInterface
+{
+    public function createTeam(array $data);
+    public function leaveTeam(Team $team, User $user);
+    public function findUserById(int $id);
+    public function checkIfUsersAreInTeam(array $members);
+    public function selectTeamsWhereIsDifferentOfMyTeam(Team $team);
+    public function listAllTeam();
+}
