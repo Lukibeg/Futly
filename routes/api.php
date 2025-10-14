2505a1fec024f706609109aa4c3a8764dc9c511d
lukibeg <lucas.santoscv25@gmail.com>
2025-10-13 22:39:05 -0300

    UPDATE: Created new routes to APIs endpoints.

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,27 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\TeamController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\RegisterController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-Route::get('/teste', [UserController::class, 'index'])->middleware('auth:sanctum');
-Route::get('/user/{user}', [UserController::class, 'show'])->middleware('auth:sanctum');
-Route::patch('/user/{user}', [UserController::class, 'update'])->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/users/unassigned', [UserController::class, 'getAvaliablePlayers']);
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{user}', [UserController::class, 'show']);
+    Route::patch('/users/{user}', [UserController::class, 'update']);
+    Route::delete('/users/{user}', [UserController::class, 'delete']);
+
+
+    Route::get('/teams', [TeamController::class, 'index']);
+    Route::post('/teams/create', [TeamController::class, 'store']);
+});
 
 Route::post('/login', [LoginController::class, 'login']);
+Route::post('/register', [RegisterController::class, 'register']);
