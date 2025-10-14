7af35c7c37bc6c9c11601f33f6ebf5d9b16f08ee
lukibeg <lucas.santoscv25@gmail.com>
2025-10-13 22:40:11 -0300

    UPDATE: Implementing getAvaliablePlayers to show all players where team_id is null.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,22 +19,6 @@ class UserController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(User $user)
