2447c88850ecf5148c9d9228ec12bd41b3bd6288
lukibeg <lucas.santoscv25@gmail.com>
2025-10-13 22:50:04 -0300

    UPDATE: Created new API methods: deleteUserData, showAvaliableUserData and new features in patchUserData.

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -9,18 +9,45 @@ class UserService
 {
     public function __construct(protected UserRepositoryInterface $userRepositoryInterface) {}
 
-
-    public function patchUserData(User $user, array $data, int $authUser)
+    public function patchUserData(User $user, array $data, int $authenticatedUser)
     {
 
-        if ($user->id !== $authUser) {
+        if ($user->id !== $authenticatedUser) {
             throw new \Exception("Você não possui permissão para realizar essa ação.", 403);
+        }
+
+        if (!$user) {
+            throw new \Exception('Usuário inativo ou inexistente.', 404);
         }
 
         if (empty($data)) {
             throw new \Exception("Campo informado inválido ou vazio, verifique e tente novamente.", 403);
         }
 
-        return $this->userRepositoryInterface->patch($user, $data);
+        return $this->userRepositoryInterface->patchUserInterface($user, $data);
+    }
+
+    public function deleteUserData(User $user, $authenticatedUser)
+    {
+        if ($user->id !== $authenticatedUser) {
+            throw new \Exception("Você não possui permissão para realizar essa ação.", 403);
+        }
+
+        if (!$user) {
+            throw new \Exception("Campo informado inválido ou vazio, verifique e tente novamente.", 403);
+        }
+
+        return $this->userRepositoryInterface->deleteUserInterface($user, $authenticatedUser);
+    }
+
+    public function showAvaliableUserData()
+    {
+        $users = $this->userRepositoryInterface->showAvaliableUserInterface();
+
+        if (empty($users)) {
+            throw new \Exception("Não há usuários sem equipe, mas você pode iniciar uma somente com seu usuário.", 204);
+        }
+
+        return $users;
     }
 }
