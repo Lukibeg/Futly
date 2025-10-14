e65ebf60de17fe9d1b0d41527e7acb421f5b2698
lukibeg <lucas.santoscv25@gmail.com>
2025-10-13 22:46:22 -0300

    UPDATE: Created new method for this implementation of interface(repository) showAvaliableUserInterface.

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -2,30 +2,36 @@
 
 namespace App\Repositories;
 
+use App\Repositories\Interface\UserRepositoryInterface;
+
 use App\Models\Team;
 use App\Models\User;
-use App\Repositories\Interface\UserRepositoryInterface;
 use Illuminate\Support\Facades\DB;
 
 class UserRepository implements UserRepositoryInterface
 {
-    public function __construct() {}
-
-
-    // public function patchUserData(User $user, array $data, int $authUser)
-    // {
-
-    //     if ($user->id !== $authUser) {
-    //         throw new \Exception('Você não possui permissão para realizar essa ação');
-    //     }
-
-    //     $this->userRepository->patch($user, $data);
-    // }
-
-    public function patch(User $user, array $data)
+    public function patchUserInterface(User $user, array $data)
     {
-        // O método update() do Eloquent já retorna true ou false.
         $user->update($data);
-        return $user; // Retorna o usuário atualizado
+        return $user;
+    }
+
+    public function deleteUserInterface(User $user, int $data)
+    {
+        $user->delete();
+        return $user;
+    }
+
+    public function showAvaliableUserInterface()
+    {
+        $users = User::select('id', 'name')
+            ->whereNull('team_id')
+            ->get();
+
+        $keyedUsers = $users->mapWithKeys(function ($user) {
+            return ['user' . $user->id => $user];
+        });
+
+        return $keyedUsers;
     }
 }
