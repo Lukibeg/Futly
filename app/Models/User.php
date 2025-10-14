dd273d146b2f72ae7cfbc25417f1f1f3d6df7ba0
lukibeg <lucas.santoscv25@gmail.com>
2025-10-13 22:44:05 -0300

    FIX: Adjusted team method.

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,6 +60,6 @@ class User extends Authenticatable
 
     public function team()
     {
-        return $this->belongsTo(Team::class, 'id');
+        return $this->belongsTo(Team::class);
     }
 }
